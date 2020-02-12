671974bdd99f3586604b8fef8d7cfb1c7dba582f
richoh86 <wsoh1986@gmail.com>
2020-02-12 20:38:39 +0900

    Initial Commit

---
+++ TimerAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TimerAnimation
+//
+//  Created by richoh86 on 2020/02/12.
+//  Copyright © 2020 richoh86. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
