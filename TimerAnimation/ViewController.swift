8a262348acb0a596fe70b52f8cd2efb07014da51
richoh86 <wsoh1986@gmail.com>
2021-04-27 20:36:08 +0900

    주석제거

--- TimerAnimation/ViewController.swift
+++ TimerAnimation/ViewController.swift
@@ -18,7 +18,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         setTimer()
-        // git test test test test
     }
     
     private func setTimer() {
