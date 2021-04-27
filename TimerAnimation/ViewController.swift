f7953ac3c1f18816a4b9ef3866778c3fe92c43cb
richoh86 <wsoh1986@gmail.com>
2021-04-27 19:53:11 +0900

    테스트 #1 커밋

--- TimerAnimation/ViewController.swift
+++ TimerAnimation/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         setTimer()
+        // git test
     }
     
     private func setTimer() {
